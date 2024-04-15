1f2d4b9f32ba5c058f189313115a64ba58d11bd9
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-04-15 00:56:38 +0000

    [Start] 4-19

---
+++ db/migrate/20240415005556_create_favorites.rb
@@ -0,0 +1,11 @@
+class CreateFavorites < ActiveRecord::Migration[6.1]
+  def change
+    create_table :favorites do |t|
+      t.integer :user_id
+      t.string :post_image_id
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
